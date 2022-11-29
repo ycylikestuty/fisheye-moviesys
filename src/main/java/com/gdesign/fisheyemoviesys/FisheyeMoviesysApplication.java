b96df61d4eba4085a17e16157b7a2209e3c43ac6
ycylikestuty <3386682886@qq.com>
2022-11-29 10:45:46 +0800

    初始化

---
+++ src/main/java/com/gdesign/fisheyemoviesys/FisheyeMoviesysApplication.java
@@ -0,0 +1,13 @@
+package com.gdesign.fisheyemoviesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisheyeMoviesysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FisheyeMoviesysApplication.class, args);
+    }
+
+}
